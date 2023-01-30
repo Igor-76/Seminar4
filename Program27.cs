aeaa7c25351c891f6da212c18c6cf7e47b2bd93f
Igor <99401363+Igor-76@users.noreply.github.com>
2023-01-30 11:43:24 +0300

    Add files via upload

---
+++ Program27.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11  82 -> 10 9012-> 12
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num, sum = 0, r;
+        Console.WriteLine("Введите цисло : ");
+        num = int.Parse(Console.ReadLine());
+        while (num != 0)
+        {
+            r = num % 10;
+            num = num / 10;
+            sum = sum + r;
+        }
+        Console.WriteLine("Сумма цифр в числе : " + sum);
+        Console.ReadLine();
+
+    }
+}
---
+++ Program29.cs
@@ -0,0 +1,26 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19] 6, 1, 33 -> [6, 1, 33]
+
+namespace ConsoleApplication2
+{
+    class Laba1
+    {
+        public static void Main(string[] args)
+        {
+            int i;
+            int[] a = new int[10];
+            Console.WriteLine("Введите массив чисел: ");
+            for (i = 0; i < 10; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine(a[i]);
+
+            }
+        }
+    }
+}
+
